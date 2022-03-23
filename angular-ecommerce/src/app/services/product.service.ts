2d66bfa12a0727f43c585e0c5af2082925e6a680
Ravi J <raviregi@gmail.com>
2022-03-24 00:26:23 +0530

    defined method getProductList

--- angular-ecommerce/src/app/services/product.service.ts
+++ angular-ecommerce/src/app/services/product.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Product} from "../common/product";
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +11,12 @@ export class ProductService {
   private baseUrl = 'http://192.168.1.3:/8080/api/products';
 
   constructor(private httpClient: HttpClient) { }
+
+  getProductList(): Observable<Product[]>{
+    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+      map(response => response._embedded.products)
+    );
+  }
 }
 
 interface GetResponse{
