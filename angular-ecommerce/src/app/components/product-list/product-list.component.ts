8dc5385d5315e05363ad4555acca14314c720a46
Ravi J <raviregi@gmail.com>
2022-03-24 00:31:48 +0530

    product variable declared

--- angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ProductService} from "../../services/product.service";
+import {Product} from "../../common/product";
 
 @Component({
   selector: 'app-product-list',
@@ -8,6 +9,7 @@ import {ProductService} from "../../services/product.service";
 })
 export class ProductListComponent implements OnInit {
 
+  products: Product[];
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
