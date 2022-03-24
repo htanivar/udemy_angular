f4e290ac5b38a41f85dbc3a0d4f9cfa2fb7b8dcf
Ravi J <raviregi@gmail.com>
2022-03-24 19:58:37 +0530

    ngOnInit subscribed for the list of products

--- angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ProductService} from "../../services/product.service";
 import {Product} from "../../common/product";
 import {ActivatedRoute} from "@angular/router";
@@ -14,10 +14,14 @@ export class ProductListComponent implements OnInit {
 
   products: Product[];
   currentCategoryId: number;
-  constructor(private productService: ProductService,private  route:ActivatedRoute) { }
 
-  ngOnInit()  {
-    this.listProducts();
+  constructor(private productService: ProductService, private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    })
   }
 
   private listProducts() {
